b8ab9e33e8c343660195d736973fdec3b4a80873
ola <dashevska.olga@gmail.com>
2019-12-03 02:31:46 +0200

    add 4  blocks

--- public/js/main.js
+++ public/js/main.js
@@ -13,3 +13,34 @@ $(function(){
         $( ".toggle-menu" ).toggleClass( "menu-on");
     });
 });
+
+$('.slider').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    prevArrow:'<button class="slick-arrow slick-prev"> <i class="fas fa-chevron-left"></i> </button>',
+    nextArrow:'<button class="slick-arrow slick-next"> <i class="fas fa-chevron-right"></i> </button>',
+    autoplay: false,
+    autoplaySpeed: 1500,
+    infinite: true,
+});
+
+$('.owl-carousel').owlCarousel({
+    items:2,
+    margin:10,
+    nav:true,
+    // merge:true,
+    loop:true,
+    video:true,
+    // lazyLoad:true,
+    // center:false,
+    videoWidth: 486, // Default false; Type: Boolean/Number
+    videoHeight: 360, // Default false; Type: Boolean/Number
+    // responsive:{
+    //     480:{
+    //         items:2
+    //     },
+    //     600:{
+    //         items:4
+    //     }
+    // }
+})
