6c0439df9f342d58fc959f88689f5260b0d91840
ola <dashevska.olga@gmail.com>
2019-12-04 23:01:33 +0200

    add all without adaptive

--- public/js/main.js
+++ public/js/main.js
@@ -64,3 +64,17 @@ $('.portfolio-slider').slick({
         });
     });
 })(jQuery);
+
+$('.nav-link-new').mPageScroll2id();
+
+$(document).ready(function() {
+    $('.popup-youtube').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+
+        fixedContentPos: false
+    });
+});
